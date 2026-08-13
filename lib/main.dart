e6f45c917d3166dfbf07bc7976f18dedfeda2868
Sanjeev143 <er.sanjeevsangral@gmail.com>
2026-08-13 23:05:09 +0530

    commit - job finder app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
-import 'ar/generative_art_background.dart';
-import 'ar/intractive_mathematical.dart';
-import 'ar/solar_grid_app.dart';
-import 'dashboards/enterprise_hrms_app.dart';
+import 'mobile_app/ai_job_finder.dart';
+import 'mobile_app/thumbnail.dart';
 
 
 
 void main() {
-  runApp(const TayaboApp());
+  runApp(const AIJobFinderApp());
   // runApp(const BankingApp());
+}
+
+
+
+class BankingApp extends StatelessWidget {
+  const BankingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Finance App',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFF8F9FA),
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+        useMaterial3: true,
+      ),
+      home: const YouTubeThumbnailWidget(),
+    );
+  }
 }
