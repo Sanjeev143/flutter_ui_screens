fb4bafcb53839f039daf8060c0e0ff8869fa51d7
Sanjeev143 <er.sanjeevsangral@gmail.com>
2026-08-18 17:06:06 +0530

    commit - luxury home stay website

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,11 @@
 
 import 'package:flutter/material.dart';
-import 'package:ui_screens_exmp/webview/3d_scrollable_website.dart';
-import 'dashboards/ai_glassmorphic_dashboard.dart';
-import 'dashboards/house_hold_ui.dart';
-import 'dashboards/light_consumption_dashboard.dart' hide EFTDashboardApp;
-import 'dashboards/platform_analytic_dashboard.dart';
+import 'package:ui_screens_exmp/webview/animated_web/animated_website.dart';
+import 'package:ui_screens_exmp/webview/gradient_app.dart';
+import 'dashboards/smart_home_dashboard.dart';
+
 
 void main() {
   // runApp(EnergyAIApp());
-  runApp(EFTDashboardApp());
+  runApp(WoodNestApp());
 }
