e1124800835747273907ca1769bb9cef2bb921f4
Sanjeev143 <er.sanjeevsangral@gmail.com>
2026-09-03 16:37:27 +0530

    commit - fintech trading app 01

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,48 @@
 
 import 'package:flutter/material.dart';
-import 'package:ui_screens_exmp/webview/animated_web/animated_website.dart';
-import 'package:ui_screens_exmp/webview/gradient_app.dart' hide MyApp;
-import 'car_dashboard/car_dashboard_ui.dart';
-import 'dashboards/hr_dashboards/hr_recruitment_dashboard.dart';
-import 'dashboards/smart_home_dashboard.dart';
-import 'mobile_app/ai_assistant_app.dart';
+import 'package:ui_screens_exmp/webview/background_animation_app.dart';
 
+import 'dashboards/finance/fintech_trading_app.dart';
+import 'dashboards/finance/relatel_finance_dashboard.dart';
+import 'mobile_app/notes_app/notes_app.dart';
+import 'mobile_app/pagination_in_app/pagination_style.dart';
+
+
+// void main() {
+//   runApp(BackGroundAnimationApp());
+// }
 
 void main() {
-  // runApp(MyApp());
-  // runApp(SaaSHRDashboard());
-  runApp(MyApp());
-  // runApp(CarDashboardApp());
+  runApp(const FintechTradingApp());
 }
+
+// void main() {
+//   runApp(const RelatelDashboardApp());
+// }
+
+// void main() {
+//   runApp(const EvernoteCloneApp());
+// }
+
+// void main() {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   runApp(
+//     const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: VideoEmbedWidget(),
+//       // home: DualVideoWebViewScreen(),
+//     ),
+//   );
+// }
+
+// void main() {
+//   runApp(const LiquidGlassApp());
+// }
+
+// void main() {
+//   runApp(const SmartHomeApp());
+// }
+
+// void main() {
+//   runApp(SmartHomeApp());
+// }
